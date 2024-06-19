0c107b6a4f755dafe124c08b5f7d024da873e60a
SamueleGalli <samuele.aggeggio@gmail.com>
2024-06-19 11:43:47 +0200

    init wordpress

--- Makefile
+++ Makefile
@@ -2,18 +2,14 @@ COMPOSE_FILE=srcs/docker-compose.yml
 WORDPRESS_DATA_DIR=/home/sgalli/data/wordpres_data
 MARIADB_DATA_DIR=/home/sgalli/data/mariadb_data
 all:
-#prima di tuttoaggiorno le immagini dei container
-#avvio docker compose dentro la cartella usando la build corretta e lo metto in background
 	docker-compose -f $(COMPOSE_FILE) pull
-	docker-compose -f $(COMPOSE_FILE) up --build -d
+	docker-compose -f $(COMPOSE_FILE) up --build -d --remove-orphans
 
 clean:
-#ferma docker e servizie rimuove le immagini
-	docker-compose -f $(COMPOSE_FILE) down
+	docker-compose -f $(COMPOSE_FILE) down --remove-orphans
 	docker image prune -a
 
 vclean:
-#rimuove volumi doker gli elenca e li rimuove e poi cancella i dati persistenti in wordpress e mariadb
-	docker-compose -f $(COMPOSE_FILE) down --volumes
-	rm -rf $(WORDPRESS_DATA_DIR)/*
-	rm -rf $(MARIADB_DATA_DIR)/*
+	docker-compose -f $(COMPOSE_FILE) down --volumes --remove-orphans
+	sudo rm -rf $(WORDPRESS_DATA_DIR)/*
+	sudo rm -rf $(MARIADB_DATA_DIR)/*
--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -1,20 +1,11 @@
-#cambia il proprietario e il gruppo di /var/lib/mysql a mysql:mysql 
 chown -R mysql:mysql /var/lib/mysql
-#controllo se la direcory /var/lib.ect esiste altrimenti avvio il servizio mysql
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     systemclt start mysql
-    #creo il database se non esite e uso il set di caratteri forntin da utf
     mysql -u root -p"$ROOT_PWD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
-    #creo l'utente del database con nome e password
     mysql -u root -p"$ROOT_PWD" -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PW';"
-    #concedo i privilegi all'utente
     mysql -u root -p"$ROOT_PWD" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
-    #applico i cambiamenti sui privilegi
     mysql -u root -p"$ROOT_PWD" -e "FLUSH PRIVILEGES;"
-    #imposto la password di root mysql a root_pw
     mysqladmin -p"$ROOT_PWD" -e root password $ROOT_PW
-    #fermo il servizio mysql
     systemclt stop mysql
 fi
-#avvio mysql in modo sicuro
 /user/bin/mysqld_safe
